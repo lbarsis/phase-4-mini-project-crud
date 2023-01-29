c8a002d1e0540c16e559512ea7ecfb80b34041fa
Levi Barsis <lbarsis@gmail.com>
2023-01-28 16:00:43 -0800

    Done with assignment

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,45 @@
+class SpicesController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :spice_not_found_response
+  
+  # GET /spices
+  def index
+    spices = Spice.all
+    render json: spices, status: :accepted
+  end
+
+  # POST /spices
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
+
+  # PATCH /spices/:id
+  def update
+    spice = find_spice
+    spice.update(spice_params)
+    render json: spice, status: :accepted
+  end
+
+  # DELETE /spices/:id
+  def destroy
+    spice = find_spice
+    spice.destroy
+    head :no_content
+  end
+
+  private
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+
+  def find_spice
+    Spice.find(params[:id])
+  end
+
+  def spice_not_found_response
+    render json: {message: 'Spice not Found'}, status: :not_found
+  end
+
+end
